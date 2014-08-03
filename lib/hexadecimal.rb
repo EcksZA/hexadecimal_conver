f94b8cfe6dd26ffe7a8e3d42745502d7bb1eab7c
Xolani Mfuko <xmfuko@gmail.com>
2014-08-03 01:09:51 -0700

    Fixed error in code. Exercise completed

--- lib/hexadecimal.rb
+++ lib/hexadecimal.rb
@@ -1,12 +1,12 @@
 def hex_converter(input)
-  hex_letters = {'a' => 10, 'b' => 11, 'c' => 12, 'd' => 13, 'e' => 14, 'a' => 16
+  hex_letters = {'a' => 10, 'b' => 11, 'c' => 12, 'd' => 13, 'e' => 14, 'f' => 16
               }
   reversed_input = input.split("").reverse
   number = 0
   index = 0
 
-  reversed_input.each |hex|
-    if hex_letters.has_key?(hex**index)
+  reversed_input.each do |hex|
+    if hex_letters.has_key?(hex)
       number += (hex_letters[hex])*(16**index)
     else
       number += (hex.to_i)*(16**index)
@@ -14,6 +14,4 @@ def hex_converter(input)
     index += 1
   end
   number
-
 end
-puts hex_converter('98ab')
--- spec/hexadecimal_spec.rb
+++ spec/hexadecimal_spec.rb
@@ -1,9 +1,8 @@
-require('rspec')
-require('hexadecimal.rb')
+require 'rspec'
+require 'hexadecimal'
 
-describe('hex_converter') do
-  it("converts a hexadecimal number of 4 digits into a base 10 number") do
-    hex_converter('98ab').should(eql(39083))
+describe "hex_converter" do
+  it "converts a hexadecimal number of 4 digits into a base 10 number" do
+    hex_converter("98ab").should(eql(39083))
   end
-
 end
